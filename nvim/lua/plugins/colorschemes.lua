ba8db8a81b6bb2028c674528911069e7f25f8962
Buck McCready <jbuckmccready@gmail.com>
2024-07-18 21:57:45 -0700

    Cleanup color schemes file, add back tokyonight

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,6 +1,13 @@
 return {
-  'navarasu/onedark.nvim',
-  'Shatur/neovim-ayu',
-  'Mofiqul/vscode.nvim',
-  'sainnhe/sonokai',
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	'navarasu/onedark.nvim',
+	'Shatur/neovim-ayu',
+	'Mofiqul/vscode.nvim',
+	'sainnhe/sonokai',
 }
