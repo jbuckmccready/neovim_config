c4c03724e24a0c43b76f1a426e64895d41297fc8
Buck McCready <jbuckmccready@gmail.com>
2024-07-21 18:03:31 -0700

    Added some more color schemes

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,5 +1,5 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "catppuccin/nvim",          name = "catppuccin", priority = 1000 },
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
@@ -10,4 +10,11 @@ return {
 	'Shatur/neovim-ayu',
 	'Mofiqul/vscode.nvim',
 	'sainnhe/sonokai',
+	{
+		'maxmx03/solarized.nvim',
+		lazy = false,
+		priority = 1000,
+	},
+	'pappasam/papercolor-theme-slim',
+	{ "ellisonleao/gruvbox.nvim", priority = 1000,     config = true,  opts = {} }
 }
