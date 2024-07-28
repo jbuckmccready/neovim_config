bbc3688d51346a6a180ad966cc8995288724da9a
Buck McCready <jbuckmccready@gmail.com>
2024-07-28 14:18:34 -0700

    Format `keymappings.lua`

--- nvim/lua/config/keymappings.lua
+++ nvim/lua/config/keymappings.lua
@@ -91,7 +91,8 @@ miniclue.setup({
 vim.keymap.set({ 'n', 'x' }, "s", "<Nop>", { silent = true })
 
 -- LSP inlay hint toggle
-vim.keymap.set({ 'n' }, '\\H', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, { desc = 'Toggle lsp inlay hints' })
+vim.keymap.set({ 'n' }, '\\H', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end,
+	{ desc = 'Toggle lsp inlay hints' })
 
 -- Map up/down arrow for autocompletion selection in command mode
 vim.keymap.set({ 'c' }, '<Up>', "<C-p>", { desc = 'Select previous' })
