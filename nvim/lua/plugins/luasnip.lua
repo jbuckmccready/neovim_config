6517b9d470b82af63d0dd8e3b4f2c0f1f0b577eb
Buck McCready <jbuckmccready@gmail.com>
2024-07-28 16:38:48 -0700

    Add `luasnip`, cleanup `cmp` setup and `vsnip`

---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,24 @@
+return {
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	-- build = "make install_jsregexp"
+	dependencies = { "rafamadriz/friendly-snippets" },
+	config = function()
+		require("luasnip.loaders.from_vscode").lazy_load()
+		local ls = require("luasnip")
+
+		vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+
+		vim.keymap.set({ "i", "s" }, "<C-E>", function()
+			if ls.choice_active() then
+				ls.change_choice(1)
+			end
+		end, { silent = true })
+
+		ls.setup()
+	end,
+}
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -3,10 +3,86 @@ return {
 	dependencies = {
 		-- cmp LSP completion
 		"hrsh7th/cmp-nvim-lsp",
-		-- cmp Snippet completion
-		"hrsh7th/cmp-vsnip",
+		-- cmp luasnip completion
+		"saadparwaiz1/cmp_luasnip",
 		-- cmp Path completion
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-buffer",
 	},
+	config = function()
+		local cmp = require('cmp')
+		cmp.setup({
+			preselect = cmp.PreselectMode.None,
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end
+			},
+			mapping = {
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+				["<up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+				["<down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.close(),
+				["<Tab>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+			},
+
+			sorting = {
+				-- Sorting priority according to order of sources (prioritizes lsp)
+				comparators = {
+					cmp.config.compare.order,
+				},
+			},
+
+			-- Installed sources
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "path" },
+				{ name = "buffer",  keyword_length = 3, max_item_count = 10 },
+			},
+			-- Border window for visual clarity
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			formatting = {
+				-- Formatting function to limit width and add icon for source
+				format = function(entry, vim_item)
+					local ELLIPSIS_CHAR = '…'
+					local MAX_LABEL_WIDTH = 100
+					local MIN_LABEL_WIDTH = 20
+					local label = vim_item.abbr
+					local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
+					if truncated_label ~= label then
+						vim_item.abbr = truncated_label .. ELLIPSIS_CHAR
+					elseif string.len(label) < MIN_LABEL_WIDTH then
+						local padding = string.rep(' ',
+							MIN_LABEL_WIDTH - string.len(label))
+						vim_item.abbr = label .. padding
+					end
+
+					local menu_icon = {
+						nvim_lsp = 'λ',
+						luasnip = '⋗',
+						buffer = 'Ω',
+						path = '🖫',
+					}
+
+					vim_item.menu = menu_icon[entry.source.name]
+					return vim_item
+				end,
+			},
+		})
+	end,
 }
