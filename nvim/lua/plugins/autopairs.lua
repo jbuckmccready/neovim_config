fdc554359264dba18f554563ae1a5f0850a8a1a2
Buck McCready <jbuckmccready@gmail.com>
2024-08-15 11:25:58 -0700

    Remove autopairs fastwrap causing newline problem

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -3,19 +3,20 @@ return {
 	event = 'InsertEnter',
 	config = function()
 		require('nvim-autopairs').setup({
-			fast_wrap = {
-				map = '<C-m>',
-				chars = { '{', '[', '(', '"', "'" },
-				pattern = [=[[%'%"%>%]%)%}%,]]=],
-				end_key = '$',
-				before_key = 'h',
-				after_key = 'l',
-				cursor_pos_before = true,
-				keys = 'qwertyuiopzxcvbnmasdfghjkl',
-				manual_position = true,
-				highlight = 'Search',
-				highlight_grey = 'Comment'
-			}
+			-- FIXME: having this enabled breaks inserting newlines with enter
+			-- fast_wrap = {
+			-- 	map = '<C-m>',
+			-- 	chars = { '{', '[', '(', '"', "'" },
+			-- 	pattern = [=[[%'%"%>%]%)%}%,]]=],
+			-- 	end_key = '$',
+			-- 	before_key = 'h',
+			-- 	after_key = 'l',
+			-- 	cursor_pos_before = true,
+			-- 	keys = 'qwertyuiopzxcvbnmasdfghjkl',
+			-- 	manual_position = true,
+			-- 	highlight = 'Search',
+			-- 	highlight_grey = 'Comment'
+			-- }
 		})
 	end
 }
